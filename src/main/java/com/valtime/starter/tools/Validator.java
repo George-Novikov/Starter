6b5363d3cadf358e91a168d00861eb650d4861e7
FateMorgan <fatemorgan@yandex.com>
2023-04-12 12:47:06 +0600

    Returned some dependencies

--- src/main/java/com/valtime/starter/tools/Validator.java
+++ src/main/java/com/valtime/starter/tools/Validator.java
@@ -5,9 +5,11 @@ import com.valtime.starter.entities.User;
 public class Validator {
 	public static boolean checkInput(User user) {
 		boolean isEmpty = true;
-		if (user.getUsername().length() > 0 && user.getPassword().length() > 0 && user.getEmail().length() > 0) {
+		
+		if (user.getUsername().length() > 0 && user.getPassword().length() > 0) {
         	isEmpty = false;
         }
+		
 		return isEmpty;
 	}
 }
